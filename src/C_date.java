9c5036882676c7324ec3ae3ae72a26f1f826c360
R3tr1c <r3tr1cs3c@gmail.com>
2022-11-12 00:35:44 +0300

    Практическая работа №1: выполнение.

--- src/C_date.java
+++ src/C_date.java
@@ -1,7 +1,32 @@
+import java.util.Date;
+import java.util.Scanner;
+
+//Выберите метод (0 для выхода):
+//1)Определить день недели;
+//2)Число дней в месяце (+ проверка на високосность);
+//3)
 public class C_date {
+    private Date inp_date = new Date();
+    public static void d_week_d_num () {
 
-    public static void d_functions () {
+    }
+    public static void day_count () {
 
-
+    }
+    public static int d_functions () {
+        int m_return = 1, mode = 0;
+        System.out.println("Практическая работа №1. Выберите входные данные:\n1) Текущая дата;\n2) Ввод с клавиатуры;");
+        Scanner mode_inp = new Scanner(System.in);
+        mode = mode_inp.nextInt();
+        switch (mode - 1){
+            case 0:
+                break;
+            case 1:
+                break;
+            default:
+                break;
+        }
+        mode_inp.close();
+        return m_return;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,32 @@
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner inp_scan = new Scanner(System.in);
-    System.out.print("Выбор практической работы (номер): ");
-    int pract_num = inp_scan.nextInt();
-
-    switch (pract_num - 1) {
-        case 0:
-            C_date.d_functions();
-            break;
-        default:
-
-            break;
-    }
+        int m_return = 0;
+        Scanner inp_scan = new Scanner(System.in);
+        System.out.println("Некое приветствие?");
+        int pract_num = 0;
+        System.out.println();
+        while (true) {
+            if (m_return != 0){ break; }
+            System.out.println("Выбор практической работы (номер) [введите 0 для выхода]:");
+            pract_num = inp_scan.nextInt();
+            switch (pract_num - 1) {
+                case -1:
+                    System.out.println("Выход...");
+                    m_return = 99;
+                    break;
+                case 0:
+                    m_return = C_date.d_functions();
+                    break;
+                default:
+                    System.out.println("Ошибка ввода! Попробуйте еще.");
+//                    inp_scan.next();
+                    break;
+            }
 //    System.out.printf("Тест принятия кода практической: %d \n", pract_num);
-    inp_scan.close();
+//            inp_scan.close();
+        }
     }
 }
